700e338c43d98aa4a5b1068d8b5ebcfab88e76fc
Claudia <Claudia@Claudia>
2017-12-13 14:28:40 +0200

    Start Project

---
+++ PROJECT/src/test/java/org/app/service/ejb/test/TestInternshipDataServiceEJBArq.java
@@ -0,0 +1,80 @@
+package org.app.service.ejb.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.ejb.EJB;
+
+import org.app.service.ejb.InternshipService;
+import org.app.service.ejb.InternshipServiceEJB;
+import org.app.service.entities.Internship;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+@RunWith(Arquillian.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestInternshipDataServiceEJBArq {
+	private static Logger logger = Logger.getLogger(TestInternshipDataServiceEJBArq.class.getName());
+	
+	@EJB
+	private static InternshipService service;
+
+	@Deployment
+	public static Archive<?> createDeployment(){
+		return ShrinkWrap
+				.create(WebArchive.class, "SCRUM-S3-test.war")
+				.addPackage(Internship.class.getPackage())
+				.addClass(InternshipService.class)
+				.addClass(InternshipServiceEJB.class)
+				.addAsResource("META-INF/persistence.xml")
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@Test
+	public void test1_GetMessage(){
+		logger.info("DEBUG: Junit TESTING: getMessage ...");
+		String response = service.getMessage();
+		assertNotNull("DataService failed!", response);
+		logger.info("DEBUG: EJB Response ..." + response);
+	}
+	
+	@Test
+	public void test4_GetInternship(){
+		logger.info("DEBUG: Junit TESTING: testetInternship ...");
+		
+		Collection<Internship> internsip = service.getInternships();
+		assertTrue("Fail to read internships!", internsip.size() > 0);
+	}
+	
+	@Test
+	public void test3_AddInternship(){
+		logger.info("DEBUG: Junit TESTING: testAddInternships ...");
+		
+		Integer internsipToAdd = 3;
+		for(int i=1; i <= internsipToAdd; i++){
+			service.addInternship(new Internship(1100, "Testare", "Trei luni", new Date(), new Date(), 1110, Internship.getListPromoInter(), Internship.getListAplInter()));
+		}
+		Collection<Internship> internsip = service.getInternships();
+		assertTrue("Fail to add internship!", internsip.size() == internsipToAdd);
+	}
+	
+	@Test
+	public void test2_DeleteInternship(){
+		logger.info("DEBUG: Junit TESTING: testDeleteInternship ...");
+		
+		Collection<Internship> internsipAfterDelete = service.getInternships();
+		assertTrue("Fail to read internship!", internsipAfterDelete.size() == 0);
+	}
+}
---
+++ PROJECT/src/test/java/org/app/service/ejb/test/TestInterviuTehnicDataServiceEJBArq.java
@@ -0,0 +1,83 @@
+package org.app.service.ejb.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.ejb.EJB;
+
+import org.app.service.ejb.InterviuTehnicService;
+import org.app.service.ejb.InterviuTehnicServiceEJB;
+import org.app.service.entities.InterviuTehnic;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+@RunWith(Arquillian.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestInterviuTehnicDataServiceEJBArq {
+	private static Logger logger = Logger.getLogger(TestInterviuTehnicDataServiceEJBArq.class.getName());
+	
+	@EJB
+	private static InterviuTehnicService service;
+
+	@Deployment
+	public static Archive<?> createDeployment(){
+		return ShrinkWrap
+				.create(WebArchive.class, "SCRUM-S3-test.war")
+				.addPackage(InterviuTehnic.class.getPackage())
+				.addClass(InterviuTehnicService.class)
+				.addClass(InterviuTehnicServiceEJB.class)
+				.addAsResource("META-INF/persistence.xml")
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@Test
+	public void test1_GetMessage(){
+		logger.info("DEBUG: Junit TESTING: testGetInterviuTehnic ...");
+		String response = service.getMessage();
+		assertNotNull("Data Service failed!", response);
+		logger.info("DEBUG: EJB Response ..." + response);
+	}
+	
+	@Test
+	public void test4_GetInterviuTehnic(){
+		logger.info("DEBUG: Junit TESTING: testGetInterviuTehnic ...");
+		
+		Collection<InterviuTehnic> inttehnic = service.getInterviuriTehnice();
+		assertTrue("Fail to read interviu tehnice!", inttehnic.size() > 0);
+	}
+	
+	@Test
+	public void test3_AddInterviuTehnic(){
+		logger.info("DEBUG: Junit TESTING: testAddInterviuTehnic ...");
+		
+		Integer inttehnicToAdd = 3;
+		for(int i=1; i <= inttehnicToAdd; i++){
+			service.addInterviuTehnic(new InterviuTehnic(1002, "Zmau Cornelia", new Date(), "Modelare", 1220, 8, "Acceptat", InterviuTehnic.getListInterInterv(), InterviuTehnic.getListAplInterv()));
+		}
+		Collection<InterviuTehnic> inttehnic = service.getInterviuriTehnice();
+		assertTrue("Fail to add Interviuri!", inttehnic.size() == inttehnicToAdd);
+	}
+	
+	@Test
+	public void test2_DeleteInterviuTehnic(){
+		logger.info("DEBUG: Junit TESTING: testDeleteInterviuTehnic ...");
+		
+		Collection<InterviuTehnic> inttehnic = service.getInterviuriTehnice();
+		for(InterviuTehnic it: inttehnic)
+			service.removeInterviuTehnic(it);
+		Collection<InterviuTehnic> inttehnicAfterDelete = service.getInterviuriTehnice();
+		assertTrue("Fail to read inttehnic!", inttehnicAfterDelete.size() == 0);
+	}
+}
